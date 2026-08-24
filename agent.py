305b3565cdf9cfed7feb6076bee1aa4b88b250f9
zyrox <183991423+Zyrox-exe@users.noreply.github.com>
2026-08-24 21:04:30 +0530

    Completed Prototype

--- agent.py
+++ agent.py
@@ -4,6 +4,10 @@ from datetime import datetime
 import speech_recognition as sr
 from random import choice
 from utils import opening_text
+import requests
+from functions.online_ops import find_my_ip, get_latest_news, get_random_advice, get_random_joke, get_trending_movies, get_weather_report, play_on_youtube, search_on_google, search_on_wikipedia, send_email, send_whatsapp_message
+from functions.os_ops import open_calculator, open_camera, open_cmd, open_notepad
+from pprint import pprint
 
 USERNAME = config('USER')
 BOTNAME = config('BOTNAME')
@@ -16,7 +20,7 @@ engine.setProperty('volume', 1.0)
 
 # Voice
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+engine.setProperty('voice', voices[0].id)
 
 # Text to Speech
 def speak(text):
@@ -41,7 +45,9 @@ def take_user_input():
     """Takes user input, recognizes it using Speech Recognition module and converts it into text"""
 
     r = sr.Recognizer()
-    with sr.Microphone as source:
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source, duration=1)
+        r.pause_threshold = 1.0
         print('Listening......')
         r.pause_threshold = 1
         audio = r.listen(source)
@@ -58,7 +64,93 @@ def take_user_input():
             else:
                 speak("Have a good day sir!")
             exit()
-        except Exception:
-            speak('Sorry, I could not understand. Could you say that again?')
-            query = 'None'
-        return query
+    except Exception:
+        speak('Sorry, I could not understand. Could you say that again?')
+        query = 'None'
+    return query
+
+if __name__ == '__main__':
+    greet_user()
+    while True:
+        query = take_user_input().lower()
+
+        if 'open notepad' in query:
+            open_notepad()
+
+        elif 'open command prompt' in query:
+            open_cmd()
+        
+        elif 'open camera' in query:
+            open_camera()
+        
+        elif 'open calculator' in query:
+            open_calculator()
+
+        elif 'ip address' in query:
+            ip_address = find_my_ip()
+            speak(f'Your IP Address is {ip_address}.\n For your convenience, I am printing it on the screen sir.')
+            print(f'Your IP Address is {ip_address}')
+        
+        elif 'wikipedia' in query:
+            speak('What do you want to search on Wikipedia, sir?')
+            search_query = take_user_input().lower()
+            results = search_on_wikipedia(search_query)
+            speak(f"According to Wikipedia, {results}")
+            speak(f"For your convenience, I am printing it on the screen sir.")
+            print(results)
+
+        elif 'youtube' in query:
+            speak(f"What do you want to watch sir?")
+            video = take_user_input().lower()
+            play_on_youtube(video)
+        
+        elif 'search on google' in query:
+            speak('What do you want to search on Google, sir?')
+            query = take_user_input().lower()
+            search_on_google(query)
+        
+        elif "send whatsapp message" in query:
+            speak('On what number should I send the message sir? Please enter in the console: ')
+            number = input("Enter the number: ")
+            speak("What is the message sir?")
+            message = take_user_input().lower()
+            send_whatsapp_message(number, message)
+            speak("I've sent the message sir.")
+        
+        elif 'send an email' in query:
+            speak("On what email address do I send sir? Please enter in the console: ")
+            reciever_address = input("Enter email address: ")
+            speak("What should be the subject sir?")
+            subject = take_user_input().capitalize()
+            if send_email(reciever_address, subject, message):
+                speak("I've sent the email sir.")
+            else:
+                speak("Something went wrong while I was sending the mail. Please check the error log sir.")
+
+        elif 'joke' in query:
+            speak(f"Hope you like this one sir")
+            joke = get_random_joke()
+            speak("For your convenience, I am printing it on the screen sir.")
+            pprint(joke)
+
+        elif "advice" in query:
+            speak(f"Here's an advice for you, sir")
+            advice = get_random_advice()
+            speak(advice)
+            speak("For your convenience, I am printing it on the screen sir")
+            pprint(advice)
+        
+        elif "trending movies" in query:
+            speak(f"Some of the trending movies are: {get_trending_movies()}")
+            speak("For your convenience I am printing it on the screen sir")
+            print(*get_trending_movies(), sep='\n')
+        
+        elif 'weather' in query:
+            ip_address = find_my_ip()
+            city = requests.get(f"https://ipapi.co/{ip_address}/city/").text
+            speak(f"Getting weather report for your city {city}")
+            weather, temperature, feels_like, = get_weather_report(city)
+            speak(f"The current temperature is {temperature}, but it feels like {feels_like}")
+            speak(f"Also, the weather report talks about {weather}")
+            speak("For your convenience, I am printing it on the screen sir.")
+            print(f"Description: {weather}\nTemperature: {temperature}\nFeels like: {feels_like}")
